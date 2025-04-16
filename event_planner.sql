dcfb20d5d0f1eff489b7525481d5b8d737881dda
Felipi2021 <filipasus9@gmail.com>
2025-04-17 01:02:54 +0200

    updated database - ratings table and account created at alternation

--- event_planner.sql
+++ event_planner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 16, 2025 at 10:11 AM
+-- Generation Time: Apr 17, 2025 at 01:01 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `event_id`, `user_id`, `text`, `created_at`) VALUES
+(1, 9, 5, 'fsf', '2025-04-16 08:25:02'),
+(3, 9, 5, 'dsfs', '2025-04-16 09:01:57'),
+(16, 9, 7, 'cos', '2025-04-16 11:42:58'),
+(17, 9, 6, 'cos', '2025-04-16 11:56:11'),
+(18, 9, 5, 'dsgd', '2025-04-16 13:13:05'),
+(19, 9, 6, 'ytsegfdydh', '2025-04-16 20:54:46'),
+(20, 9, 9, 'cos', '2025-04-16 22:03:19'),
+(21, 10, 9, 'cos', '2025-04-16 22:03:57'),
+(22, 10, 5, 'fsdfsd', '2025-04-16 22:32:49');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `events`
 --
 
@@ -39,6 +68,14 @@ CREATE TABLE `events` (
   `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `title`, `description`, `date`, `location`, `capacity`, `attendees_count`, `created_by`, `image`) VALUES
+(9, 'test1', 'opis testu', '2025-12-12', 'poznan', 3, 3, 5, '1744791279695-bronik.png'),
+(10, 'fsfdsffs', 'ffasfaf', '2222-02-22', 'saffa', 12, 1, 9, '1744841025781-amstaff.png');
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +87,43 @@ CREATE TABLE `favorites` (
   `user_id` int(11) NOT NULL,
   `event_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `favorites`
+--
+
+INSERT INTO `favorites` (`id`, `user_id`, `event_id`) VALUES
+(20, 5, 9),
+(22, 6, 9),
+(23, 7, 9),
+(27, 9, 9),
+(26, 9, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL,
+  `rater_id` int(11) NOT NULL,
+  `rated_id` int(11) NOT NULL,
+  `rating` decimal(2,1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ratings`
+--
+
+INSERT INTO `ratings` (`id`, `rater_id`, `rated_id`, `rating`, `created_at`) VALUES
+(1, 5, 9, 4.0, '2025-04-16 22:32:53'),
+(4, 6, 9, 4.0, '2025-04-16 22:33:22'),
+(5, 9, 5, 1.5, '2025-04-16 22:45:20'),
+(6, 9, 7, 5.0, '2025-04-16 22:48:23'),
+(12, 6, 5, 4.0, '2025-04-16 22:54:45'),
+(13, 6, 7, 5.0, '2025-04-16 22:57:18');
 
 -- --------------------------------------------------------
 
@@ -63,6 +137,16 @@ CREATE TABLE `registration` (
   `event_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`id`, `user_id`, `event_id`) VALUES
+(57, 7, 9),
+(58, 6, 9),
+(61, 5, 9),
+(62, 9, 10);
+
 -- --------------------------------------------------------
 
 --
@@ -75,12 +159,33 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('user','admin') DEFAULT 'user',
-  `image` varchar(255) DEFAULT NULL
+  `image` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `image`, `description`, `created_at`) VALUES
+(5, 'a', 'a@a.a', '$2a$10$XTm8QSPNP5UrtfZjerAYF.eLaMRpVaaBC3g/NAvwcSvijy5QiUMnG', 'user', '1744791246225-amstaff.png', '', '2025-04-16 21:19:52'),
+(6, 'b', 'b@b.b', '$2a$10$s9SvgXSJqNty2FGhpOdQDe0XwliEQY/xm.oSdJdx6bKBb.6FJppIu', 'user', '1744791925550-pieski.png', '', '2025-04-16 21:19:52'),
+(7, 'abc', 'c@c.c', '$2a$10$fhWFQ6cgMZOF2uPnmX6uuOX2zd6lQmoBLZU5Z1Wi8SsH0shlp.r0a', 'user', '1744803648563-pomaranca.png', NULL, '2025-04-16 21:19:52'),
+(8, 'xxx', 'x@x.x', '$2a$10$L5WcdJTTYDUMv5Bwk8BgRO.RyrucGwdbOgYJUbLLiTma/rsgeCene', 'user', '1744838511749-pomaranca.png', NULL, '2025-04-16 21:21:51'),
+(9, 'v', 'v@v.v', '$2a$10$9XaAa5IFe3hEwMPI9GnD2.X3BQ4Vy0iaZhpIbgJ1lipgSJZTjIq86', 'user', '1744838703480-amstaff.png', 'erwettwt', '2025-04-16 21:25:03');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `event_id` (`event_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `events`
@@ -95,6 +200,14 @@ ALTER TABLE `favorites`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `user_event_unique` (`user_id`,`event_id`),
   ADD KEY `event_id` (`event_id`);
+
+--
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_rater_rated` (`rater_id`,`rated_id`),
+  ADD KEY `rated_id` (`rated_id`);
 
 --
 -- Indexes for table `registration`
@@ -116,32 +229,51 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `favorites`
 --
 ALTER TABLE `favorites`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `favorites`
@@ -149,6 +281,13 @@ ALTER TABLE `users`
 ALTER TABLE `favorites`
   ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`rater_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ratings_ibfk_2` FOREIGN KEY (`rated_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `registration`
